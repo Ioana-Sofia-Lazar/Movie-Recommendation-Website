4c6c9b72b817310f8234cd606a4765700298d4f8
Ioana-Sofia Pascu <ioana.pascu9596@yahoo.com>
2018-03-13 21:09:36 +0200

    Pages Basic Design

--- server.js
+++ server.js
@@ -11,18 +11,78 @@ var params = {
     userLoggedIn: true,
     movieInWatchlist: true,
     movieSeen: false,
-    profilePage: ""
+    profilePage: "",
+    genres: [   
+        {
+            name: 'Comedy',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Drama',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Romance',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Action',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Thriller',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Sci-Fi',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Mistery',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Documentary',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Animated',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Horror',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Crime',
+            photo: 'comedy.jpg'
+        },
+        {
+            name: 'Fantasy',
+            photo: 'comedy.jpg'
+        }
+
+    ], 
+    periods: [   
+        {
+            name: '1970s',
+            photo: 'comedy.jpg'
+        }
+    ]
 };
+
 
 // index page 
 app.get('/', function(req, res) {
     res.render('index', params);
 });
 
+// recommendations
 app.get('/recommendations', (req, res) => {
     res.render('recommendations', params);
 });
 
+// view movie details
 app.get('/viewMovie', (req, res) => {
     res.render('view-movie', params);
 });
@@ -35,6 +95,26 @@ app.get('/watchlist', (req, res) => {
 // seen movies (can also be viewed outside profile page)
 app.get('/seen', (req, res) => {
     res.render('seen', params);
+});
+
+// explore
+app.get('/explore', (req, res) => {
+    res.render('explore', params);
+});
+
+// search
+app.get('/search', (req, res) => {
+    res.render('search', params);
+});
+
+// community
+app.get('/community', (req, res) => {
+    res.render('community', params);
+});
+
+// movies by category (genre/time period)
+app.get('/explore/byCategory', (req, res) => {
+    res.render('by-category', params);
 });
 
 // profile
