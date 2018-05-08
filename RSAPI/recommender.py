59ca9ae2f380d2ec2f04fe2cf6ceb3dc9f84a8fa
y0rg1 <y0rg1@github.com>
2018-05-08 22:27:41 +0300

    now using latest datasets

--- RSAPI/recommender.py
+++ RSAPI/recommender.py
@@ -2,39 +2,45 @@ import io
 
 from surprise import KNNBaseline
 from surprise import Dataset
-from surprise import get_dataset_dir
+from surprise import Reader
+from surprise import dump
 
 
 def read_item_names():
-    file_name = get_dataset_dir() + '/ml-100k/ml-100k/u.item'
+    file_name = './datasets/movies_w_imdbids.csv' # get_dataset_dir() + '/ml-100k/ml-100k/u.item'
     rid_to_name = {}
     name_to_rid = {}
-    with io.open(file_name, 'r', encoding='ISO-8859-1') as f:
+    with io.open(file_name, 'r') as f:
         for line in f:
-            line = line.split('|')
+            line = line.split(';')
             rid_to_name[line[0]] = line[3]
             name_to_rid[line[3]] = line[0]
 
     return rid_to_name, name_to_rid
 
+# reader = Reader(line_format='user item rating timestamp', sep=',')
+# data = Dataset.load_from_file('./datasets/ratings.csv', reader=reader)
+# trainset = data.build_full_trainset()
+# sim_options = { 'name': 'pearson_baselineclear', 'user_based': False }
+# algo = KNNBaseline(sim_options=sim_options)
+# algo.fit(trainset)
 
-data = Dataset.load_builtin('ml-100k')
-trainset = data.build_full_trainset()
-sim_options = {'name': 'pearson_baseline', 'user_based': False}
-algo = KNNBaseline(sim_options=sim_options)
-algo.fit(trainset)
+# dump.dump('./knn.algo', algo=algo)
+
+_, algo = dump.load('./knn.algo')
+print('Loaded knn algorithm')
 
 rid_to_name, name_to_rid = read_item_names()
 
 def get_similar_movies(imdb_id, num):
-	if imdb_id in name_to_rid.keys():
-		raw_id = algo.trainset.to_inner_iid(name_to_rid[imdb_id])
-		neighbors = algo.get_neighbors(raw_id, k=num)
-		neighbors = (algo.trainset.to_raw_iid(inner_id)
-                	 for inner_id in neighbors)
-		neighbors = (rid_to_name[rid]
+    if imdb_id in name_to_rid.keys():
+        raw_id = algo.trainset.to_inner_iid(name_to_rid[imdb_id])
+        neighbors = algo.get_neighbors(raw_id, k=num)
+        neighbors = (algo.trainset.to_raw_iid(inner_id)
+                     for inner_id in neighbors)
+        neighbors = (rid_to_name[rid]
                      for rid in neighbors)
-	else:
-		neighbors = []
+    else:
+        neighbors = []
 
-	return neighbors
+    return neighbors
